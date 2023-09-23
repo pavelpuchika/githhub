eb8e3cee6f0ea1cbb33d10cbfd84375200eddae5
pavelpuchika <animationpuchikation@gmail.com>
2023-09-23 15:38:28 +0200

    add

--- index.py
+++ index.py
@@ -1,10 +1,2 @@
-import matplotlib.pyplot as plt
-
-import matplotlib.patches as mpatches
-
-fig, ax = plt.subplots()
-red_patch = mpatches.Patch(color='red', label='The red data')
-ax.legend(handles=[red_patch])
-
-plt.show()
+print("print is not working:(")
 
