f340ef23a7252c62765fcdaf5d0410e15c058b26
pavelpuchika <animationpuchikation@gmail.com>
2023-09-23 15:59:16 +0200

    add

--- index.py
+++ index.py
@@ -1,2 +1,11 @@
-print("print is not working:(")
+class person:
+    __a=[1,2,4,3,2,1,-40,30,85,-1]
+    __b=[1,3,22,-67,35,34,68,2,3,4]
+    def m(self):
+        a=sum(self.__a)
+        b=sum(self.__b)
+        print(a,b)
+        print(a>b)
+i=person()
+i.m()
 
