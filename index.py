24991fe61b1409248b28f3b5c0de80b8be6f7039
pavelpuchika <animationpuchikation@gmail.com>
2023-09-23 15:35:57 +0200

    add

--- index.py
+++ index.py
@@ -1,1 +1,10 @@
-print('hello world')
+import matplotlib.pyplot as plt
+
+import matplotlib.patches as mpatches
+
+fig, ax = plt.subplots()
+red_patch = mpatches.Patch(color='red', label='The red data')
+ax.legend(handles=[red_patch])
+
+plt.show()
+
